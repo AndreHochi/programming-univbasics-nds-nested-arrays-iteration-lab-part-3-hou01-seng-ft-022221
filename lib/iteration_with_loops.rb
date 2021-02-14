fb7608cafa8b6e9c24544040be1f914fb29112c6
Andre Hochi <andrehochiwork@gmail.com>
2021-02-14 02:52:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index]
+      if src[row_index][element_index].kindof?
         big_string = big_string + src[row_index][element_index].to_s
       element_index = element_index + 1
     end
